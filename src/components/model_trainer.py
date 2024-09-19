dd716d9fdab6ddb7306d2e89199c79d7c7896ebc
ashikacp <ashikacp2012@gmail.com>
2024-09-19 17:13:40 +0100

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,7 +46,54 @@ class ModelTrainer():
                 "AdaBoost Regression":AdaBoostRegressor()
             } 
 
-            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params={
+
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256],
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None]
+                },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{
+                    'fit_intercept': [True, False]
+                },
+                "KNeighbors Regression": {
+                     'n_neighbors': [3, 5, 7, 9],
+                     'weights': ['uniform', 'distance'],
+                     'metric': ['euclidean', 'manhattan']
+                },
+                "XGB Regression":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost Regression":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regression":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+
+            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -65,10 +112,11 @@ class ModelTrainer():
                 file_path = self.model_trainer_config.trained_model_file_path,
                 obj = best_model #dumping the best model in model.pkl file
             )
-
+            #import pandas as pd; print(pd.read_pickle('artifacts/model.pkl'))
             predicted = best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
+            
             return r2_square
 
 
